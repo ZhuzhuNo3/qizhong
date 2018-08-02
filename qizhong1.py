bcfc22e478f6e8365df8f321a1e9a4f9059a659f
ZhuzhuNo3 <907130054@qq.com>
2018-08-02 23:31:48 +0800

    *

--- qizhong1.py
+++ qizhong1.py
@@ -1,25 +1,28 @@
 import requests,re,threading
 
-def get_pic(pagenum=1):
-
+def get_piclist(pagenum=1):
     url = 'https://www.qiushibaike.com/imgrank/page/%d/'%(pagenum)
     result = requests.get(url)
     data = result.text
     #print(data)
     pic = re.findall(r'img src="(//\S+)" alt="糗事',data)
     #print(pic)
-    for i in pic:
-        url_pic = 'https:' + i
-        #print(i)
-        picname = re.findall(r'/(\w+\.\w+)$',i)[0]
-        #print(picname)
-        try:
-            picb = requests.get(url_pic).content
-            with open(picname,'wb') as f:
-                f.write(picb)
-            print('%s 下载成功'% picname)
-        except:
-            print('%s 下载失败'% picname)
+    for x in pic:
+        tx = threading.Thread(target=get_pic,args=(x,),name=x)
+        tx.start()
+
+def get_pic(i):
+    url_pic = 'https:' + i
+    #print(i)
+    picname = re.findall(r'/(\w+\.\w+)$',i)[0]
+    #print(picname)
+    try:
+        picb = requests.get(url_pic).content
+        with open(picname,'wb') as f:
+            f.write(picb)
+        print('%s 下载成功'% picname)
+    except:
+        print('%s 下载失败'% picname)
 
 print('请输入起始页：',end='')
 stapage = int(input())
@@ -27,6 +30,6 @@ print('请输入终止页：',end='')
 endpage = int(input())
 print('开始下载')
 for i in range(stapage,endpage+1):
-    tx = threading.Thread(target=get_pic,args=(i,))
-    tx.start()
-
+    ti = threading.Thread(target=get_piclist,args=(i,),name=i)
+    ti.start()
+    
